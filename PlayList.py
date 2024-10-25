1f38546778326da92e58a8fb77b5885740976c35
1709846 <146718293+clarapriego@users.noreply.github.com>
2024-10-25 19:56:18 +0200

    actualitzada i comprovada PlayList.py

--- PlayList.py
+++ PlayList.py
@@ -1,13 +1,14 @@
 
 # CLASSE PLAYLIST
 
-
 from VideoID import VideoID
+from VideoFIles import VideoFiles
 import uuid
 from VideoPlayer import VideoPlayer
 from VideoData import *
 import cfg
 import vlc
+import os
 
 class PlayList():
     """
@@ -20,13 +21,26 @@ class PlayList():
 
     def __init__(self, videos=None):
         if videos is None:
-            self._videos = []
+            self.videos = []
         else:
-            self._videos = videos
+            self.videos = videos
 
-
-        
-
+        self._root = cfg.get_root()
+    
+        self._v_files = VideoFiles(self._root)
+        self._v_id = VideoID()
+        self._v_data = VideoData()
+    
+        videos_afegits = self._v_files.files_added()
+        for path in videos_afegits:
+            self._v_id.generate_uuid(path)
+            uuid = self._v_id.get_uuid(path)
+            self._v_data.add_video(uuid, path)
+            self._v_data.load_metadata(uuid)
+        self._v_files.reload_fs(self._root)
+    
+        self._v_player = VideoPlayer(self._v_data, self._v_id)
+                
         #enllaçar llistes
         self._next = None
 
@@ -35,68 +49,83 @@ class PlayList():
         
         for linia in f:
                 
-            if not linia.startswith("#") and linia.endswith(".mp4"):   # ignorar les linies
-                uuid_video = VideoID.get_uuid(linia)
+            if not linia.startswith("#") and linia.endswith(".mp4"):   # saltar linies que son comentaris
+                self._v_id.generate_uuid(linia)
+                uuid_video = self._v_id.get_uuid(linia)
+                self._v_data.add_video(uuid_video, linia)
+                self._v_data.load_metadata(uuid_video)
+                self._v_files.reload_fs(self._root)
                     
-                if uuid:
-                    self._videos.append(uuid_video)
+                if uuid_video:
+                    self.videos.append(uuid_video)
 
 
-    def play(self, videodata, video_id) -> None:
-        for video in self._videos:
-            player = VideoPlayer(videodata, video_id)
-            player.play_video(str(video), cfg.PLAY_MODE)
+    def play(self) -> None:
+        for video in self.videos:
+            self._v_player.play_video(video, cfg.PLAY_MODE)
             
         # playlist enllaçada
-        if self._next_playlist is not None:
-            self._next_playlist.play()
+        if self._next is not None:
+            self._next.play()
         
 
     def add_video_at_end(self, uuid: str) -> None:
-        self._videos.append(uuid)
+        self.videos.append(uuid)
 
     def remove_first_video(self) -> None:
-        self._videos.pop(0)
+        self.videos.pop(0)
 
     def remove_last_video(self) -> None:
-        self._videos.pop(-1)
-    
+        self.videos.pop(-1)
     
     # enllaçar llistes
-    def _next_playlist(self, playlist):
+    def next_playlist(self, playlist):
         self._next = playlist
         
 
 
 def main():
     # Crear una instancia de PlayList
+    print("Creant playlist...")
     playlist = PlayList()
+    print("Load_file...")
+    #playlist.load_file("/Users/clara/Documentos/2º GED/ED/projecte/#EXTM3U.txt")
     
-    print("Generant UUIDs...")
-    video1_path = "/Users/clara/Documentos/2º GED/ED/projecte/corpus/Beyond/2575_galaxy_Space_Milky_Way_GalaxyWithCustomization720p5000br.mp4"
-    video2_path = "/Users/clara/Documentos/2º GED/ED/projecte/videos/Doraemon Opening 1 (Català) (360p).mp4"
-   
-    video_id = VideoID()
-    # Generar UUIDs
-    uuid1 = video_id.generate_uuid(video1_path)
-    uuid2 = video_id.generate_uuid(video2_path)
+    root = cfg.get_root()
 
-    video_data = VideoData()
-
-    # metadades
-    video_data.add_video(uuid1, video1_path)
-    video_data.add_video(uuid2, video2_path)
-
-    # Afegir UUIDs a la playlist
+    v_files = VideoFiles(root)
+    v_id = VideoID()
+    v_data = VideoData()
+    
+    
+    videos_afegits = v_files.files_added()
+    for path in videos_afegits:
+        v_id.generate_uuid(path)
+        uuid = v_id.get_uuid(path)
+        v_data.add_video(uuid, path)
+        v_data.load_metadata(uuid)
+    v_files.reload_fs(root)
+    
     print("Afegint videos a la playlist...")
-    playlist.add_video_at_end(uuid1)
+    uuid = v_id.get_uuid('/Users/clara/Documentos/2º GED/ED/projecte/corpus/Big data/_Cyberpunk55.mp4')
+    playlist.add_video_at_end(uuid)
+    uuid2 = v_id.get_uuid('/Users/clara/Documentos/2º GED/ED/projecte/corpus/Big data/579180_matrix_computer_cyber_DataStardustBinaryLoop4K720p5000br.mp4')
     playlist.add_video_at_end(uuid2)
+
 
     # Reproducció
     print("Reproduint videos...")
-    playlist.play(video_data, video_id)
+    playlist.play()
+
+    print("Enllaçant amb una segona playlist...")
+    p = PlayList([uuid, uuid2])
+    playlist.next_playlist(p)
+
+    print("Reproduint amb l'enllaç...")
+    playlist.play()
 
     print("Final!")
 
-main()
-
+if __name__ == "__main__":
+    main()
+        
