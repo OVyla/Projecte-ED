7269215571430a40f2e9cba1b55e09cc6482aaf2
NIU1702761 <146651281+NIU1702761@users.noreply.github.com>
2024-11-18 11:07:16 +0100

    18 nov, 11:07

--- VideoFIles.py
+++ VideoFIles.py
@@ -1,8 +1,3 @@
-import cfg
-import os
-import shutil
-
-
 # -*- coding: utf-8 -*-
 """
 VideoFiles.py : ** REQUIRED ** El vostre codi de la classe VideoFiles.
@@ -19,7 +14,7 @@ class VideoFiles:
                     dels MP4 existents dins la col·lecció de vídeos.
     """
 
-    def __init__(self, root:str):
+    def __init__(self, root:cfg.get_root()):
         self._root = root
         self._files = []
         self._added = []
@@ -66,64 +61,7 @@ class VideoFiles:
                 self._removed.append(file)
         return self._removed
     
-    def __str__(self):
+    def __str__(self) -> str:
         cad = '________VIDEO FILES_____\n'
         cad += 'Num fitxers llegits: '+str(len(self._files))
         return cad
-
-"""
-def main():
-    # Inicialitzem la classe VideoFiles
-    vf = VideoFiles()
-
-    # Carreguem el directori arrel
-    root_dir = cfg.get_root()
-    vf.reload_fs(root_dir)
-
-    # Mostrar la llista d'arxius afegits
-    print('Primer reload fet...')
-    print()
-    
-    # Afegir un nou fitxer (heb_2.mp4)
-    print('Afegint heb_2.mp4...')
-    shutil.copyfile('/Users/aliciamartilopez/Desktop/ED/PROJECTE/P0/Videos/heb3070b1_1241630.mp4',
-                    '/Users/aliciamartilopez/Desktop/ED/PROJECTE/P0/Videos/heb_2.mp4')
-    print('Fitxers afegits (vf.files_added()):', vf.files_added())
-    print()
-
-    # Eliminar un fitxer (Doraemon Opening)
-    print('Eliminant Doraemon Opening 1 (Català) (360p).mp4...')
-    with open('/Users/aliciamartilopez/Desktop/ED/PROJECTE/P0/Videos/Doraemon Opening 1 (Català) (360p).mp4', 'rb') as video_file:
-        video_eliminat = video_file.read()
-    os.remove('/Users/aliciamartilopez/Desktop/ED/PROJECTE/P0/Videos/Doraemon Opening 1 (Català) (360p).mp4')
-    print('Fitxers eliminats (vf.files_removed()):', vf.files_removed())
-    print()
-
-    # Tornem a carregar el sistema de fitxers i comprovem canvis
-    print('Nou reload...')
-    vf.reload_fs(root_dir)
-    print('Fitxers afegits després del reload:', vf.files_added())
-    print('Fitxers eliminats després del reload:', vf.files_removed())
-    print()
-    print()
-    
-    # Tornem a deixar tot com estava (eliminar heb_2.mp4 i recrear Doraemon i finalment carregar el reload)
-    print('Tornant tot a l\'estat original...')
-    os.remove('/Users/aliciamartilopez/Desktop/ED/PROJECTE/P0/Videos/heb_2.mp4')
-    print('Eliminat heb_2.mp4 (vf.files_removed()):', vf.files_removed())
-
-    with open('/Users/aliciamartilopez/Desktop/ED/PROJECTE/P0/Videos/Doraemon Opening 1 (Català) (360p).mp4', 'wb') as new_video_file:
-        new_video_file.write(video_eliminat)
-    print('Recreat Doraemon (vf.files_added()):', vf.files_added())
-    print('Carregant els canvis fent reload...')
-    vf.reload_fs(root_dir)
-    print('Fitxers afegits després del reload:', vf.files_added())
-    print('Fitxers eliminats després del reload:', vf.files_removed())
-    print()
-    print("Finalitzat!")
-
-
-if __name__ == "__main__":
-    main()
-
-"""
