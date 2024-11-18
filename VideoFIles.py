cba7e4962df30f8445d974271d81790d109e0308
NIU1702761 <146651281+NIU1702761@users.noreply.github.com>
2024-11-18 11:12:54 +0100

    VideoFiles definitiu

--- VideoFIles.py
+++ VideoFIles.py
@@ -4,8 +4,7 @@ VideoFiles.py : ** REQUIRED ** El vostre codi de la classe VideoFiles.
 """
 import cfg
 import os
-#import shutil
-
+import copy
 
 class VideoFiles:
     """
@@ -14,11 +13,11 @@ class VideoFiles:
                     dels MP4 existents dins la col·lecció de vídeos.
     """
 
-    def __init__(self, root:cfg.get_root()):
-        self._root = root
+    def __init__(self):
         self._files = []
         self._added = []
         self._removed = []
+        
     
     def get_files(self) -> list:
         return self._files
@@ -30,38 +29,42 @@ class VideoFiles:
         """
         Carrega el sistema de fitxers i actualitza la llista d'arxius MP4.
         """
-        self._root = path
+        files_anterior = copy.deepcopy(self._files)
+        added_anterior = copy.deepcopy(self._added)
+        removed_anterior = copy.deepcopy(self._removed)
         self._files = []
         self._added = []
         self._removed = []
-
+        
+        
         for root, dirs, files in os.walk(path):
             for filename in files:
                 if filename.lower().endswith('.mp4'):
                     self._files.append(os.path.join(root, filename))
+        
+        for file in self._files:
+            if file not in files_anterior:
+                self._added.append(file)
+        
+        for file in files_anterior:
+            if file not in self._files:
+                self._removed.append(file)
+                
 
     def files_added(self) -> list:
         """
         Retorna una llista dels arxius MP4 que han estat afegits des de l'últim reload.
         """
-        for root, dirs, files in os.walk(self._root):
-            for filename in files:
-                if filename.lower().endswith('.mp4'):
-                    full_path = os.path.join(root, filename)
-                    if full_path not in self._files and full_path not in self._added:
-                        self._added.append(full_path)
         return self._added
 
     def files_removed(self) -> list:
         """
         Retorna una llista dels arxius MP4 que han estat eliminats des de l'últim reload.
         """
-        for file in self._files:
-            if not os.path.isfile(file) and file not in self._removed:
-                self._removed.append(file)
         return self._removed
     
-    def __str__(self) -> str:
+    def __str__(self):
         cad = '________VIDEO FILES_____\n'
         cad += 'Num fitxers llegits: '+str(len(self._files))
         return cad
+                                           
