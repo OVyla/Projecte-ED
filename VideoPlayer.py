afba51cbcfe14cba9bc687ba477c3642b3728b9d
1709846 <146718293+clarapriego@users.noreply.github.com>
2024-11-18 11:36:23 +0100

    VideoPlayer definitiu

--- VideoPlayer.py
+++ VideoPlayer.py
@@ -1,10 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 16 16:29:17 2024
-
-@author: aliciamartilopez
-"""
 
 import vlc
 import time
@@ -12,6 +7,7 @@ import cfg
 import VideoData 
 import VideoFiles 
 import VideoID 
+import os
 
 class VideoPlayer:
     def __init__(self, video_data):
@@ -28,12 +24,17 @@ class VideoPlayer:
         print("Comentari:", self._video_data.get_comment(uuid))
 
     def play_file(self, file: str) -> None:
-        
+        if not os.path.isfile:
+            return None
+        if file not in self._video_data._files:
+            return None
         uuid = cfg.get_uuid(cfg.get_canonical_pathfile(file))
         duracio = self._video_data.get_duration(uuid)
-        
-        player = vlc.MediaPlayer(file)
-        player.play()
+        try:
+            player = vlc.MediaPlayer(file)
+            player.play()
+        except Exception:
+            return None
     
         timeout = time.time() + duracio
         while True:
@@ -41,47 +42,31 @@ class VideoPlayer:
                 try:
                     time.sleep(1)
                 except KeyboardInterrupt:  # STOP amb <CTRL>+<C> a la consola
-                    break
+                    return None
             else:
-                break
+                return None
         
         player.stop()
+        
+        return player
 
     def play_video(self, uuid: str, mode: int) -> None:
+        if uuid not in self._video_data._files:
+            return None
+        if not isinstance(mode, int):
+            return None
+            
         if mode < 2:
             self.print_video(uuid)
         if mode > 0:
             file = self._video_data.get_filename(uuid)
-            self.play_file(file)
+            return self.play_file(file)
+            
+        return None
+            
+            
     
     def __str__(self):
-        pass
-"""
-def main():
-    root = cfg.get_root()
-    
-    v_files = VideoFiles(root)
-    v_id = VideoID()
-    v_data = VideoData()
-    
-    videos_afegits = v_files.files_added()
-    for path in videos_afegits:
-        v_id.generate_uuid(path)
-        uuid = v_id.get_uuid(path)
-        v_data.add_video(uuid, path)
-        v_data.load_metadata(uuid)
-    v_files.reload_fs(root)
-    
-    v_player = VideoPlayer(v_data, v_id)
-    
-    print('Print Video heb307...630.mp4')
-    uuid = v_id.get_uuid('/Users/aliciamartilopez/Desktop/ED/PROJECTE/P0/Videos/heb3070b1_1241630.mp4')
-    uuid2 = v_id.get_uuid('/Users/aliciamartilopez/Desktop/ED/PROJECTE/P0/Videos/Les Tres Bessones - Opening HD (720p).mp4')
-    
-    mode = cfg.PLAY_MODE  # Això ho agafes de la configuració
-    v_player.play_video(uuid2, mode)
-    
+        return f"VideoPlayer: {len(self._video_data)} vídeos disponibles."
 
-if __name__ == "__main__":
-    main()
-"""
+
