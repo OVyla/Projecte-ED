6221f2c780c54317aaef79f4f993826de33cfc04
1709846 <146718293+clarapriego@users.noreply.github.com>
2024-12-24 16:42:12 +0100

    VideoData modificat pero no complet

--- Part_2/VideoData.py
+++ Part_2/VideoData.py
@@ -1,135 +1,142 @@
 # -*- coding: utf-8 -*-
 import cfg
-import os
-import sys
-import numpy
+from GrafHash import GrafHash
+from ElementData import ElementData
+
 import tinytag
-import PlayList
-from collections.abc import Iterator
+import numpy
+import os
 
 class VideoData:
+
     __slots__ = ['_files']
 
+
     def __init__(self):
-        self._files={}
+        self._files = GrafHash()
 
-    def add_video(self, uuid, file):
-        ll_files = [v[0] for v in self._files.values()]
-        if file not in ll_files:
-            path = os.path.join(os.path.join(cfg.get_root(), file))
-            self._files[uuid] = [file,path]
 
-    def remove_video(self, uuid):
-        if uuid in self._files.keys(): 
-            del self._files[uuid]
+    def add_video(self, uuid: str, file: str):
+        if uuid not in self._files:
+            try:
+               
+                path = os.path.join(cfg.get_root(), file)
+                if os.path.isfile(path):
+                    element = ElementData(filename=file)
+                    self._files.insert_vertex(uuid, element)
+
+            except FileNotFoundError:
+                print("ERROR")
+
 
     def load_metadata(self, uuid):
-        if uuid in self._files.keys(): 
-            path = self._files[uuid][1]
-            metadata = tinytag.TinyTag.get(path)
-            if metadata is None:
-                print("ERROR: Arxiu MP4 erroni!")
-                sys.exit(1)
+        if uuid in self._files:
+            element = self._files.get(uuid)
             try:
-                self._files[uuid].append(numpy.ceil(metadata.duration))
 
-            except AttributeError:
-                duration = -1
-            
-            try:
-                self._files[uuid].append(metadata.title)
-            except AttributeError:
-                title = "None"
-         
-            try:
-                self._files[uuid].append(metadata.album)
-            except AttributeError:
-                album = "None"
+                path = os.path.join(cfg.get_root(), element.filename)
+                metadata = tinytag.TinyTag.get(path)
 
-            try:
-                self._files[uuid].append(metadata.artist)
-            except AttributeError:
-                artist = "None"
-            
-            try:
-                self._files[uuid].append(metadata.composer)
-            except AttributeError:
-                composer = "None"
-            
+                if metadata is None:
+                    print(f'Error al afegir el video {uuid}')
 
-            try:
-                self._files[uuid].append(metadata.genre)
+                else:
+                    element.title=metadata.title or None,
+                    element.artist=metadata.artist or None,
+                    element.album=metadata.album or None,
+                    element.composer=metadata.composer or None,
+                    element.genre=metadata.genre or None,
+                    element.date=metadata.year or None,
+                    element.comment=metadata.comment or None,
+                    element.duration=int(numpy.ceil(metadata.duration)) if metadata.duration else -1,
+                    
+                
             except AttributeError:
-                genre = "None"
-            
-            try:
-                self._files[uuid].append(metadata.year)
-            except AttributeError:
-                date = "None"
-            
-            try:
-                self._files[uuid].append(metadata.comment)
-            except AttributeError:
-                comment = "None"
-            
- 
+                print(f'ERROR al carregar les dades: {e}')
+
+
+
+    def remove_video(self, uuid: str):
+        if uuid in self._files:
+            del self._files[uuid]
+
+
+    def get_metadata(self, uuid):
+        if uuid in self._files:
+            return self._files[uuid]
+        return None
+
+    def get_all_videos(self):
+        return self._files.get_uuids()
+    
+    
+    def existeix_meta(self, uuid):
+        return self._files.get(uuid).title is not None
+    
+    def get_filename(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).filename
+        return None
+
+
+    def get_path(self, uuid):
+        if self.existeix_meta(uuid):
+            return os.path.join(cfg.get_root(), self._files[uuid].uuid())
+        return None
+
+    def get_duration(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).duration
+        return None
+        
+    def get_title(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).title
+        return None
+        
+    def get_album(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).album
+        return None
+        
+    def get_artist(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).artist
+        return None
+        
+    def get_composer(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).composer
+        return None
+        
+    def get_genre(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).genre
+        return None
+        
+    def get_date(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).date
+        return None
+        
+    def get_comment(self, uuid):
+        if self.existeix_meta(uuid):
+            return self._files.get(uuid).comment
+        return None
+        
+    def files(self):
+        return {uuid: self._files.get(uuid) for uuid in self._files}
+
+    def get_uuids(self) -> list:
+        return list(self._files.vertices())
+
+    
+
     def __len__(self):
         return len(self._files)
 
-    def existeix_meta(self,uuid):
-        if uuid in self._files.keys(): 
-            return len(self._files[uuid]) > 2
-       
-    def get_filename(self,uuid):
-        if self.existeix_meta(uuid):
-            return str(self._files[uuid][0])
-   
-    def get_path(self,uuid):
-        if self.existeix_meta(uuid):
-            return self._files[uuid][1]
-   
-    def get_duration(self,uuid):
-        if self.existeix_meta(uuid):
-            return self._files[uuid][2]
-       
-    def get_title(self,uuid):
-        if self.existeix_meta(uuid):
-            return str(self._files[uuid][3])
-       
-    def get_album(self,uuid):
-        if self.existeix_meta(uuid):
-            return str(self._files[uuid][4])
-       
-    def get_artist(self,uuid):
-        if self.existeix_meta(uuid):
-            return str(self._files[uuid][5])
-       
-    def get_composer(self,uuid):
-        if self.existeix_meta(uuid):
-            return str(self._files[uuid][6])
-       
-    def get_genre(self,uuid):
-        if self.existeix_meta(uuid):
-            return str(self._files[uuid][7])
-       
-    def get_date(self,uuid):
-        if self.existeix_meta(uuid):
-            return str(self._files[uuid][8])
-
-    def get_comment(self,uuid):
-        if self.existeix_meta(uuid):
-            return str(self._files[uuid][9])
-   
-    def files(self):
-        return self._files
-    
-
-    def get_uuids(self) -> list:
-        """Retorna tots els UUIDs disponibles."""
-        return list(self._files.keys())
-
     def __repr__(self):
-        return f"VideoData({len(self._files)} vídeos)"
+        return f"VideoData({len(self)} videos)"
 
     def __iter__(self):
         return iter(self._files)
@@ -140,21 +147,30 @@ class VideoData:
         return self._files == other._files
 
     def __hash__(self):
-        return hash(tuple(self._files))
-    
+        return hash(self._files)
+
     def __ne__(self, other):
         return not self.__eq__(other)
-    
+
     def __lt__(self, other):
         return len(self._files) < len(other._files)
+
+    def __str__(self):
+        return str(self._files)
     
-    def read_playlist(self, obf_playlist: PlayList):
+    def read_playlist(self, playlist):
         pass
-    
-    def get_video_rank(self, uuid: str) -> int:
+
+    def get_video_rank(self, uuid):
         pass
-    
-    def get_next_videos(self, uuid: str):
+
+    def get_next_videos(self, uuid):
+        pass
+
+    def get_previous_videos(self, uuid):
+        pass
+
+    def get_video_distance(self, uuid1, uuid2):
         pass
     
     def get_previous_videos(self,uuid: str):
